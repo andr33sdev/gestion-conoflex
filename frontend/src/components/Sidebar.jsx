b10770a04cefb02103b9e8d45a441a0953cd1187
Andres Cardoso <andr33sdev@gmail.com>
2024-12-19 20:50:57 -0300

    adjusting Sidebar tailwind and DetalleMateriaPrima visuals

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -6,35 +6,66 @@ const Sidebar = () => {
   const location = useLocation(); // Obtenemos la ruta actual con `useLocation`
 
   return (
-    <div className="flex flex-col my-auto ml-5 p-5 bg-zinc-800 rounded-xl" style={{ height: "95vh", width: "20vw" }}>
-      <div className="ml-8">
+    <div
+      className="flex flex-col my-auto ml-5 p-5 bg-zinc-800 rounded-xl"
+      style={{ height: "95vh", width: "20vw" }}
+    >
+      <div className="mx-auto">
         <img src={logo} alt="logo flexcore" className="w-64 mt-3" />
       </div>
-      <nav className="mt-12 ml-8 text-white">
+      <nav className="mt-12 text-white">
         <ul className="space-y-5">
-          <li className={location.pathname === "/dashboard" ? "bg-purple-400 rounded-lg w-48 font-bold p-3" : "p-3"}>
+          <li
+            className={
+              location.pathname === "/dashboard"
+                ? "bg-purple-500 rounded-lg w-48 font-bold p-3"
+                : "p-3 hover:bg-purple-400 hover:rounded-lg hover:w-48 hover:cursor-pointer"
+            }
+          >
             <Link className="link" to="/dashboard">
               <span className="mr-3">📊</span> Dashboard
             </Link>
           </li>
           <li
-            className={location.pathname === "/materia-prima" ? "bg-purple-400 rounded-lg w-48 font-bold p-3" : "p-3"}
+            className={
+              location.pathname === "/materia-prima"
+                ? "bg-purple-500 rounded-lg w-48 font-bold p-3"
+                : "p-3 hover:bg-purple-400 hover:rounded-lg hover:w-48 hover:cursor-pointer"
+            }
           >
             <Link className="link" to="/materia-prima">
               <span className="mr-3">🪣</span> Materia Prima
             </Link>
           </li>
-          <li className={location.pathname === "/matrices" ? "bg-purple-400 rounded-lg w-48 font-bold p-3" : "p-3"}>
+          <li
+            className={
+              location.pathname === "/matrices"
+                ? "bg-purple-500 rounded-lg w-48 font-bold p-3"
+                : "p-3 hover:bg-purple-400 hover:rounded-lg hover:w-48 hover:cursor-pointer"
+            }
+          >
             <Link className="link" to="/matrices">
               <span className="mr-3">⛓️</span> Matrices
             </Link>
           </li>
-          <li className={location.pathname === "/store" ? "bg-purple-400 rounded-lg w-48 font-bold p-3" : "p-3"}>
+          <li
+            className={
+              location.pathname === "/store"
+                ? "bg-purple-500 rounded-lg w-48 font-bold p-3"
+                : "p-3 hover:bg-purple-400 hover:rounded-lg hover:w-48 hover:cursor-pointer"
+            }
+          >
             <Link className="link" to="/store">
               <span className="mr-3">🏪</span> Store
             </Link>
           </li>
-          <li className={location.pathname === "/visitor" ? "bg-purple-400 rounded-lg w-48 font-bold p-3" : "p-3"}>
+          <li
+            className={
+              location.pathname === "/visitor"
+                ? "bg-purple-500 rounded-lg w-48 font-bold p-3"
+                : "p-3 hover:bg-purple-400 hover:rounded-lg hover:w-48 hover:cursor-pointer"
+            }
+          >
             <Link className="link" to="/visitor">
               <span className="mr-3">👥</span> Visitor
             </Link>
--- frontend/src/pages/MateriaPrima/DetalleMateriaPrima.jsx
+++ frontend/src/pages/MateriaPrima/DetalleMateriaPrima.jsx
@@ -26,31 +26,31 @@ const DetalleMateriaPrima = () => {
   }
 
   return (
-    <div className="main-content">
-      <div className="container materia-prima-dashboard">
-        <div className="materia-prima-card-data">
+    <div className="text-white pt-8 px-20 w-full h-screen">
+      <div className=" grid grid-cols-4 grid-rows-4 gap-5">
+        <div className="bg-purple-400 p-2 rounded-lg">
           Código <p>{materiaPrima.code}</p>
           <button>Editar</button>
         </div>
-        <div className="materia-prima-card-data">
+        <div className="bg-purple-400 p-2 rounded-lg">
           <p>Proveedores</p>
           <button>Editar</button>
         </div>
-        <div className="materia-prima-card-data">
+        <div className="bg-purple-400 p-2 rounded-lg">
           Stock <p>{materiaPrima.stock}</p>
           <button>Editar</button>
         </div>
-        <div className="materia-prima-card-data">
+        <div className="bg-purple-400 p-2 rounded-lg">
           Consumos promedio
           <p>Semanal:</p>
           <p>Mensual:</p>
           <p>Anual:</p>
         </div>
         <div className="materia-prima-table materia-prima-dashboard">
-          <table>
+          <table className="table-fixed bg-zinc-800 rounded-lg w-full text-center text-slate-200">
             <thead>
-              <tr>
-                <th>Código</th>
+              <tr className="border-b-2 border-zinc-700 uppercase">
+                <th className="p-2">Código</th>
                 <th>Stock</th>
                 <th>Stock mínimo</th>
                 <th>Última modificación</th>
