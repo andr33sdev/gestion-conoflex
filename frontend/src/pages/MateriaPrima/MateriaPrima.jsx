25205008feacc82dc4ee4f54252c6e7c24e3cd90
Andres Cardoso <andr33sdev@gmail.com>
2024-12-21 20:08:47 -0300

    testing

--- frontend/src/pages/MateriaPrima/MateriaPrima.jsx
+++ frontend/src/pages/MateriaPrima/MateriaPrima.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import moment from "moment-timezone";
+// Prueba
 
 const MateriaPrima = () => {
   const [materiasPrimas, setMateriasPrimas] = useState([]);
